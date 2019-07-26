3144acc37fa711e786d5ee7fd5f0ffb5e1d26bd5
dinghu <dinghu@fkhwl.com>
2019-07-26 16:04:15 +0800

    案例

--- app/src/main/java/com/qing/guo/decoration/ui/activity/CaseDetailActivity.java
+++ app/src/main/java/com/qing/guo/decoration/ui/activity/CaseDetailActivity.java
@@ -1,9 +1,36 @@
 package com.qing.guo.decoration.ui.activity;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.fkh.support.ui.activity.BaseActivity;
 import com.qing.guo.decoration.R;
+import com.qing.guo.decoration.entity.resp.Site;
+import com.qing.guo.decoration.utils.AppUtils;
+import com.zhy.view.flowlayout.TagFlowLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CaseDetailActivity extends BaseActivity {
+
+
+    @BindView(R.id.icon)
+    ImageView icon;
+    @BindView(R.id.name)
+    TextView name;
+    @BindView(R.id.price)
+    TextView price;
+    @BindView(R.id.tagLayout)
+    TagFlowLayout tagLayout;
+    @BindView(R.id.desc)
+    TextView desc;
+    @BindView(R.id.webView)
+    WebView webView;
+
     @Override
     protected int getLayout() {
         return R.layout.activity_case_detail;
@@ -11,6 +38,9 @@ public class CaseDetailActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        Site site = (Site) getIntent().getSerializableExtra("case");
+        name.setText(site.getTitle());
+        Glide.with(this).load(AppUtils.getImageUrl(site.getImg())).into(icon);
+        webView.loadDataWithBaseURL(null, site.getDetail(), "text/html", "utf-8", null);
     }
 }
--- app/src/main/java/com/qing/guo/decoration/ui/activity/CaseListActivity.java
+++ app/src/main/java/com/qing/guo/decoration/ui/activity/CaseListActivity.java
@@ -1,19 +1,41 @@
 package com.qing.guo.decoration.ui.activity;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
+import com.blankj.utilcode.util.ToastUtils;
+import com.bumptech.glide.Glide;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.qing.guo.decoration.R;
 import com.qing.guo.decoration.base.BaseRefreshLoadListViewActivity;
+import com.qing.guo.decoration.entity.resp.ListResp;
+import com.qing.guo.decoration.entity.resp.Site;
+import com.qing.guo.decoration.retrofit.ResponseListener;
+import com.qing.guo.decoration.service.impl.ApiServiceImpl;
 import com.qing.guo.decoration.utils.ActivityUtils;
+import com.qing.guo.decoration.utils.AppUtils;
 
-public class CaseListActivity extends BaseRefreshLoadListViewActivity<Integer, CaseListActivity.ViewHolder> {
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CaseListActivity extends BaseRefreshLoadListViewActivity<Site, CaseListActivity.ViewHolder> {
+    static
+
     public class ViewHolder {
-        private RoundedImageView image;
+        @BindView(R.id.image)
+        RoundedImageView image;
+        @BindView(R.id.scan_num)
+        TextView scanNum;
+        @BindView(R.id.title)
+        TextView title;
 
-        public ViewHolder(View view) {
-            image = (RoundedImageView) view.findViewById(R.id.image);
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
         }
     }
 
@@ -24,13 +46,13 @@ public class CaseListActivity extends BaseRefreshLoadListViewActivity<Integer, C
 
     @Override
     protected void initView() {
-        mDatas.add(1);
-        mDatas.add(2);
         super.initView();
         getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ActivityUtils.startActivity(CaseListActivity.this,CaseDetailActivity.class);
+                Intent intent = new Intent(CaseListActivity.this, CaseDetailActivity.class);
+                intent.putExtra("case",mDatas.get(i));
+                ActivityUtils.startActivity(CaseListActivity.this,intent);
             }
         });
     }
@@ -41,17 +63,31 @@ public class CaseListActivity extends BaseRefreshLoadListViewActivity<Integer, C
     }
 
     @Override
-    public CaseListActivity.ViewHolder getViewHolder(View convertView) {
-        return null;
+    public ViewHolder getViewHolder(View convertView) {
+        return new ViewHolder(convertView);
     }
 
     @Override
-    public void initializeViews(int position, Integer integer, CaseListActivity.ViewHolder viewHolder) {
-
+    public void initializeViews(int position, Site site, ViewHolder viewHolder) {
+        viewHolder.title.setText(site.getTitle());
+        Glide.with(this).load(AppUtils.getImageUrl(site.getImg())).into(viewHolder.image);
     }
 
     @Override
     public void getData(int page, boolean isRefreh) {
+        Map<String, String> params = new HashMap<>();
+        params.put("rate", "Y");
+        ApiServiceImpl.getSiteList(params, new ResponseListener<ListResp<Site>>() {
+            @Override
+            public void onSuccess(ListResp<Site> siteListResp) {
+                dealDataRecive(siteListResp.list, true);
+            }
 
+            @Override
+            public void onFail(String code, String message) {
+                ToastUtils.showLong(message);
+            }
+        });
     }
+
 }
