57331da46703d1c2a0980fbe5f511945d726fe60
dinghu <dinghu@fkhwl.com>
2019-09-26 14:58:01 +0800

    工地筛选

--- app/src/main/java/com/qing/guo/decoration/Constant/UrlConstant.java
+++ app/src/main/java/com/qing/guo/decoration/Constant/UrlConstant.java
@@ -2,7 +2,12 @@ package com.qing.guo.decoration.Constant;
 
 public class UrlConstant {
 
-    public static String baseUrl = "http://mir150.123nat.com:8090/";
+//    后台：http://mir150.123nat.com:8088/console/login
+//
+//    图片：http://mir150.123nat.com:8088/file/upload/user/20190717/201907171013456797.jpg
+//    接口：http://mir150.123nat.com:8088/app/product/list
+
+    public static String baseUrl = "http://mir150.123nat.com:8088/";
     public static String rongyunUrl = "https://api-cn.ronghub.com";
-    public static String imageBaseUrl = "http://mir150.123nat.com:8090";
+    public static String imageBaseUrl = "http://mir150.123nat.com:8088/";
 }
